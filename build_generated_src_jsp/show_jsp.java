ed7c7a0179921b0e2075702b3d18546f817186ae
SejalAsati17 <133386243+SejalAsati17@users.noreply.github.com>
2025-11-28 08:09:51 +0530

    Add files via upload

---
+++ build_generated_src_jsp/show_jsp.java
@@ -0,0 +1,120 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import java.sql.ResultSet;
+import db.DBConnector;
+import java.sql.Statement;
+
+public final class show_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("    <style>\n");
+      out.write("        #wishlist{\n");
+      out.write("             width: 280px;\n");
+      out.write("             height: 300px;\n");
+      out.write("             border-radius: 10px;\n");
+      out.write("             background-color: gray;\n");
+      out.write("             color: white;\n");
+      out.write("             padding: 10px;\n");
+      out.write("        }\n");
+      out.write("    </style>\n");
+      out.write("    <body>\n");
+      out.write("       \n");
+      out.write("          ");
+
+             Statement st = DBConnector.getStatement();
+             ResultSet rs=null;
+            
+             String query = ("Select product from addto" );
+             
+             try
+             {
+             rs= st.executeQuery(query);
+             if(rs.next())
+              {
+              
+      out.write(" \n");
+      out.write("        \n");
+      out.write("               <div id=\"wishlist\">\n");
+      out.write("                   <h3>My wish list</h3>\n");
+      out.write("                   \n");
+      out.write("                          <h4>");
+      out.print(rs.getString(1));
+      out.write("</h4> \n");
+      out.write("                          \n");
+      out.write("               </div>\n");
+      out.write("              \n");
+      out.write("                  \n");
+      out.write("                                  ");
+}
+              }
+              catch(Exception e)
+                {
+                    System.out.println(e); 
+                }
+              
+      out.write("\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
